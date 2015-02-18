0d4c5db76d64b0f5b0c1e3331f8e75f3659a476d
Roxanne Therese Luangco <roxanneluangco@gmail.com>
2015-02-18 17:11:17 +0800

    Generate models/City.php

---
+++ practice/raluangco/basic/models/City.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "city".
+ *
+ * @property integer $id
+ * @property string $city_code
+ * @property string $city_description
+ * @property integer $province_id
+ *
+ * @property Province $province
+ */
+class City extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'city';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['province_id'], 'required'],
+            [['province_id'], 'integer'],
+            [['city_code', 'city_description'], 'string', 'max' => 32]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'city_code' => Yii::t('app', 'City Code'),
+            'city_description' => Yii::t('app', 'City Description'),
+            'province_id' => Yii::t('app', 'Province ID'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProvince()
+    {
+        return $this->hasOne(Province::className(), ['id' => 'province_id']);
+    }
+}
