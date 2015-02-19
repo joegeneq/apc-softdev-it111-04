a063bb9c9745cb7e731d442e17954b5f47e89054
Arianne Bianca Papna <biapapna@gmail.com>
2015-02-19 22:34:18 +0800

    Updated db.php

--- application/JMGTCC/config/db.php
+++ application/JMGTCC/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=jmgtcc',
+    'dsn' => 'mysql:host=localhost;dbname=jmgtccdb',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
