7a147f4657ff8c4d772e8d187020dd4b5305d430
Roxanne Therese Luangco <roxanneluangco@gmail.com>
2015-02-28 10:42:40 +0800

    Updated database connection

--- practice/midterm_test/Luangco_Roxanne_Therese_midterms/oursource/config/db.php
+++ practice/midterm_test/Luangco_Roxanne_Therese_midterms/oursource/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=softdev_luangco_roxannetherese_ourspace',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
