a834e89273d16418c37a530b8f27d56938df0d08
Roxanne Therese Luangco <roxanneluangco@gmail.com>
2015-03-01 14:10:43 +0800

    Change myaddress_id textfield to lastname dropdown

--- practice/midterm_test/Luangco_Roxanne_Therese_midterms/ourspace/views/Mycomment/_form.php
+++ practice/midterm_test/Luangco_Roxanne_Therese_midterms/ourspace/views/Mycomment/_form.php
@@ -3,6 +3,8 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
+use yii\helpers\ArrayHelper;
+use app\models\Myaddress;
 /* @var $this yii\web\View */
 /* @var $model app\models\Mycomment */
 /* @var $form yii\widgets\ActiveForm */
@@ -14,8 +16,10 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'id')->textInput() ?>
 
-    <?= $form->field($model, 'myaddress_id')->textInput() ?>
-
+    <?= $form->field($model, 'myaddress_id')->dropDownList (
+		ArrayHelper::map(Myaddress::find()->all(),'id', 'lastname'),
+			['prompt'=>'Select Lastname']);
+	?>
     <?= $form->field($model, 'author')->textInput(['maxlength' => 255]) ?>
 
     <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
