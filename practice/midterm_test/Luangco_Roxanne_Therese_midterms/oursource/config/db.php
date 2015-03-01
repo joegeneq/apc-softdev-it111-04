687710f65ea3c6ae360c7777a405ebcc4e0eeecd
Roxanne Therese Luangco <roxanneluangco@gmail.com>
2015-03-01 13:15:11 +0800

    Change folder name oursource to ourspace

--- practice/midterm_test/Luangco_Roxanne_Therese_midterms/oursource/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=softdev_luangco_roxannetherese_ourspace',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
