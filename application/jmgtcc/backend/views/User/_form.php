02710c0d5442a8bcf1445b9e5df808770e88fcb4
Erika Hidalgo <rekhidalgo23@gmail.com>
2015-03-06 17:55:01 +0800

    Generated model and CRUD for User

---
+++ application/jmgtcc/backend/views/User/_form.php
@@ -0,0 +1,49 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\User */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="user-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'username')->textInput(['maxlength' => 25]) ?>
+
+    <?= $form->field($model, 'first_name')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'last_name')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'gender')->textInput(['maxlength' => 1]) ?>
+
+    <?= $form->field($model, 'city')->textInput(['maxlength' => 45]) ?>
+
+    <?= $form->field($model, 'contact_number')->textInput(['maxlength' => 20]) ?>
+
+    <?= $form->field($model, 'auth_key')->textInput(['maxlength' => 32]) ?>
+
+    <?= $form->field($model, 'password_hash')->textInput(['maxlength' => 255]) ?>
+
+    <?= $form->field($model, 'password_reset_token')->textInput(['maxlength' => 255]) ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => 255]) ?>
+
+    <?= $form->field($model, 'status')->textInput() ?>
+
+    <?= $form->field($model, 'created_at')->textInput() ?>
+
+    <?= $form->field($model, 'updated_at')->textInput() ?>
+
+    <?= $form->field($model, 'last_logged_in')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
