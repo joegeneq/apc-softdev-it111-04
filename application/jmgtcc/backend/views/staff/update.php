696b6bec0ed42929d9473f9e50a94adc8668521b
Erika Hidalgo <rekhidalgo23@gmail.com>
2015-03-06 20:10:56 +0800

    Regenerated CRUD for staff

---
+++ application/jmgtcc/backend/views/staff/update.php
@@ -0,0 +1,23 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Staff */
+
+$this->title = Yii::t('app', 'Update {modelClass}: ', [
+    'modelClass' => 'Staff',
+]) . ' ' . $model->id;
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Staff'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id, 'roles_id' => $model->roles_id]];
+$this->params['breadcrumbs'][] = Yii::t('app', 'Update');
+?>
+<div class="staff-update">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
