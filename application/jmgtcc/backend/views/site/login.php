dbf9e8c222948c9e9e45b6d920901e49b882d188
Arianne Bianca Papna <biapapna@gmail.com>
2015-03-08 23:31:28 +0800

    Updated homepage / login pages for backend

--- application/jmgtcc/backend/views/site/login.php
+++ application/jmgtcc/backend/views/site/login.php
@@ -9,24 +9,31 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'Login';
 ?>
 <div class="site-login">
-    
-    <br>
 
+     <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+      
     <div class="login-form-main">
        <div class="login-form">
-            <div class="login-form-form">
-            <br>
-                <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-                    <?= $form->field($model, 'username') ?>
-                    <?= $form->field($model, 'password')->passwordInput() ?>
-                    <?= $form->field($model, 'rememberMe')->checkbox() ?>
-                    <div class="form-group">
-                        <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                    </div>
-                <?php ActiveForm::end(); ?>
-           
-             <br>
+            <div class="login-form-form">               
+
+                <?= $form->field($model, 'username')
+                        ->label(false) 
+                        ->textInput(['placeholder' => 'Username'])
+                    ?>
+
+                <?= $form->field($model, 'password')
+                        ->passwordInput(['placeholder' => 'Password'])
+                        ->label(false) ?> 
+                
+                <?= $form->field($model, 'rememberMe')->checkbox() ?>               
+                <div class="form-group">
+                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                </div>
+                        <?php ActiveForm::end(); ?>
+                <br>
+
             </div>
         </div>
     </div>
+   
 </div>
