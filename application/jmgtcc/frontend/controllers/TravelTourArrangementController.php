607deaae06fbc37187c12f893d8f2728dfd8053c
Roxanne Therese Luangco <roxanneluangco@gmail.com>
2015-04-10 15:05:17 +0800

    Updated email format to be received by travel agents

--- application/jmgtcc/frontend/controllers/TravelTourArrangementController.php
+++ application/jmgtcc/frontend/controllers/TravelTourArrangementController.php
@@ -133,17 +133,10 @@ class TravelTourArrangementController extends Controller
                     ->setTo(ArrayHelper::map(Personnel::find()->all(), 'email', 'email'))
                     ->setSubject('JMGTCC CLIENT TRAVEL TOUR ARRANGEMENT ' )
                     ->setHtmlBody("<br>
-                         <div>
-                             <img style='padding-top: 10px; padding-left: 20px;' height='50' width='180'
-                             src='http:journeysglobaltours.com/wp-content/uploads/2012/11/main_logo.png' >
-                             <p style='font-size: 11px; font-family: arial; padding-bottom: 8px; padding-top: 8px; padding-left: 20px;'>
-                                 Upper Ground 12 Cityland Pioneer Condominium 128 Pioneer St., <br>
-                                 Mandaluyong City, Philippines
-                            </p>     
-                            <p style='font-family:arial; margin-left:5%;'>
-                                Listed below are the details of a new Tours Arrangement:
-                            </p>                  
-                            <table style='margin-left:10%;font-family: arial;'>
+                     <p style='font-family:arial; margin-left:5%;'>
+                            Listed below are the details of a new Tour Arrangement:
+                            <br><br>
+                            <table style='margin-left:5%;font-family: arial;'>
                                 <tr>
                                     <td width='200px' style='padding-bottom: 5px; padding-top: 5px;'><b>Client Name:</b></td>
                                     <td>".yii::$app->user->identity->first_name." ".yii::$app->user->identity->last_name."</td>
@@ -156,105 +149,98 @@ class TravelTourArrangementController extends Controller
                                     <td width='200px' style='padding-bottom: 5px; padding-top: 5px;'><b>Email Address:</b></td>
                                     <td>".yii::$app->user->identity->email."</td>
                                 </tr>
-                              </table>                 
-                             </div> 
+                            </table>    
+                        </p>
+                         <div style='width: 750px; margin-left:8%;'>
+                            _______________________________________________________________________
+                        </div>
+                        <br><br>
+                        <div style='border: 1px solid black; width: 640px; margin-left:8%;'>
+                        <div>
+                            <img style='padding-top: 10px; padding-left: 80px;' height='50' width='180'
+                            src='http://journeysglobaltours.com/wp-content/uploads/2012/11/main_logo.png' >
+                            <b style='font-size: 30px; font-family: arial; float: right; padding-right: 50px; padding-top: 70px;'>
+                                ".$model->arrangement_code."</b>
+                        </div>       
+                        <p style='font-size: 11px; font-family: arial; padding-bottom: 8px; padding-top: 8px; padding-left: 70px;'>
+                                Upper Ground 12 Cityland Pioneer Condominium 128 Pioneer St., <br>
+                                Mandaluyong City, Philippines
+                        </p>            
+                        <div style='width: 550px; margin-left:70px; '>
+                           <table style='font-family: arial'>
+                            <tr>
+                                <td width='200px' style='padding-bottom: 5px; padding-top: 5px;'><b>Place of Origin:</b></td>
+                                <td>".$model->place_of_origin."</td>
+                            </tr>
+                            <tr>
+                                <td width='200px' style='padding-bottom: 5px; padding-top: 5px;'><b>Destination:</b></td>
+                                <td>".$model->destination."</td>
+                            </tr>
+                             <tr>
+                                <td width='200px' style='padding-bottom: 5px; padding-top: 5px;'><b>Arrival Date:</b></td>
+                                <td>".$model->departure_date."</td>
+                            </tr>
+                            <tr>
+                                <td width='200px' style='padding-bottom: 5px; padding-top: 5px;'><b>Return Date:</b></td>
+                                <td>".$model->return_date."</td>
+                            </tr>
+                            <tr>
+                                <td width='200px' style='padding-bottom: 5px; padding-top: 5px;'><b>Number of PAX:</b></td>
+                                <td>".$model->number_of_pax."</td>
+                            </tr> <tr>
+                                <td width='200px' style='padding-bottom: 5px; padding-top: 5px;'><b>Hotel Name:</b></td>
+                                <td>".$model->hotel_name."</td>
+                            </tr> <tr>
+                                <td width='200px' style='padding-bottom: 5px; padding-top: 5px;'><b>Room Type:</b></td>
+                                <td>".$model->room_type."</td>
+                            </tr>
+                            </tr> <tr>
+                                 <td width='200px' style='padding-bottom: 5px; padding-top: 5px;'><b>Inclusions:</b></td>
+                                <td>
+                                    <table>
+                                        <tr>
+                                            <td>Food: </td>
+                                            <td>".$model->inclusion_food_deals."</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Tour Type: </td>
+                                            <td>".$model->inclusion_tour_type."</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Transport Services: </td>
+                                            <td>".$model->inclusion_transport_service."</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Others: </td>
+                                            <td>".$model->inclusion_freebies."</td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr> 
+                            <tr>
+                                <td width='200px' style='padding-bottom: 5px; padding-top: 3px;'><b>Remarks:</b></td>
+                                <td>".$model->remarks."</td>
+                            </tr>
+                        </table>
+                        <br>
+                        </div>
+                    </div>
 
-                             <div style='width: 700px; margin-left:5%;'>
-                                 _______________________________________________________________________
-                             </div>
-                             <br>
-                         <div style='width: 500px; margin-left:10%;'>    
-                             <b style='font-size: 30px; font-family: arial; float: right; padding-right: 0px; padding-top: 15px;'>
-                             ".$model->arrangement_code."</b>    
-                             <br><br><br><br>
-                             <div style='width: 435px; margin-left:30px; '>
-                                
-                                 <table style='font-family: arial'>
-                                     <tr>
-                                         <td width='200px' style='padding-bottom: 5px; padding-top: 5px;'><b>Place of Origin:</b></td>
-                                         <td>".$model->place_of_origin."</td>
-                                     </tr>
-                                     <tr>
-                                         <td width='200px' style='padding-bottom: 5px; padding-top: 5px;'><b>Destination:</b></td>
-                                         <td>".$model->destination."</td>
-                                     </tr>
-                                      <tr>
-                                         <td width='200px' style='padding-bottom: 5px; padding-top: 5px;'><b>Departure Date:</b></td>
-                                         <td>".$model->departure_date."</td>
-                                     </tr>
-                                     <tr>
-                                         <td width='200px' style='padding-bottom: 5px; padding-top: 5px;'><b>Return Date:</b></td>
-                                         <td>".$model->return_date."</td>
-                                     </tr>
-                                     <tr>
-                                         <td width='200px' style='padding-bottom: 5px; padding-top: 5px;'><b>Airline Name:</b></td>
-                                         <td>".$model->airline_name."</td>
-                                     </tr>
-                                      <tr>
-                                         <td width='200px' style='padding-bottom: 5px; padding-top: 5px;'><b>Flight Type:</b></td>
-                                         <td>".$model->flight_type."</td>
-                                     </tr> <tr>
-                                         <td width='200px' style='padding-bottom: 5px; padding-top: 5px;'><b>Class Type:</b></td>
-                                         <td>".$model->class_type."</td>
-                                     </tr> <tr>
-                                         <td width='200px' style='padding-bottom: 5px; padding-top: 5px;'><b>Number of PAX:</b></td>
-                                         <td>".$model->number_of_pax."</td>
-                                     </tr> <tr>
-                                         <td width='200px' style='padding-bottom: 5px; padding-top: 5px;'><b>Hotel Name:</b></td>
-                                         <td>".$model->hotel_name."</td>
-                                     </tr> <tr>
-                                         <td width='200px' style='padding-bottom: 5px; padding-top: 5px;'><b>Room Type:</b></td>
-                                         <td>".$model->room_type."</td>
-                                     </tr>
-                                     </tr> <tr>
-                                         <td width='200px' style='padding-bottom: 5px; padding-top: 5px;'><b>Inclusions:</b></td>
-                                         <td>
-                                             <table>
-                                                 <tr>
-                                                     <td>Food: </td>
-                                                     <td>".$model->inclusion_food_deals."</td>
-                                                 </tr>
-                                                 <tr>
-                                                     <td>Tour Type: </td>
-                                                     <td>".$model->inclusion_tour_type."</td>
-                                                 </tr>
-                                                 <tr>
-                                                     <td>Transport Services: </td>
-                                                     <td>".$model->inclusion_transport_service."</td>
-                                                 </tr>
-                                                 <tr>
-                                                     <td>Others: </td>
-                                                     <td>".$model->inclusion_freebies."</td>
-                                                 </tr>
-                                             </table>
-                                         </td>
-                                     </tr> 
-                                     <tr>
-                                         <td width='200px' style='padding-bottom: 5px; padding-top: 3px;'><b>Remarks:</b></td>
-                                         <td>".$model->remarks."</td>
-                                     </tr>
-                                 </table>
-                             </div>
-                             <br>
-                         </div>
-                         <div style='width: 700px; margin-left:5%;'>
-                         _______________________________________________________________________
-                         </div>          
-                
-                         <br><br>
-
-                         <div style='margin-left: 5%;'>
-                             <b style='color:red; font-family: arial; font-size: 18px'>Important Reminders:</b>
-                             <br>
-                             <br>
-                             <ul style='font-family:arial'>
-                                 <li>Please confirm the travel arrangement of the client as soon as possible</li>
-                                 <br>
-                                <li>Any updates or revisions regarding travel and arrangements can be done through email negotiations. </li>
-                             </ul>
-                         </div>
-                         <br><br>
-                         <br><br>")
+                    <br><br>
+                     <hr width='500'>
+                    <div style='margin-left: 5%;'>
+                        <b style='color:red; font-family: arial; font-size: 18px'>Important Reminders:</b>
+                        <br>
+                       
+                        <br>
+                         <ul style='font-family:arial'>
+                    <li>Please confirm the tour arrangement of the client as soon as possible</li>
+                    <br>
+                    <li>Any updates or revisions regarding the tour arrangements can be done through email negotiations. </li>
+                </ul>
+            </div>
+                <br><br>
+                <br><br>")
                     ->send();
                     Yii::$app->session->setFlash('travel-notif', 'Your request has been sent to our Travel Agent. 
                         Your Travel Quote will be sent to your email shortly. Thank you!');
