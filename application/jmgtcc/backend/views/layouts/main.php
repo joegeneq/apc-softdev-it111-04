5fc049d82a9e8e2f32775f25e5424c6406853873
Arianne Bianca Papna <biapapna@gmail.com>
2015-04-10 22:43:41 +0800

    Fixed appointment index, user view and layout

--- application/jmgtcc/backend/views/layouts/main.php
+++ application/jmgtcc/backend/views/layouts/main.php
@@ -74,6 +74,8 @@ AppAsset::register($this);
         </div>
     </div>
 
+    
+
     <footer class="footer">
         <div class="container">
         <p class="pull-left">&copy; Journeys & More Global Tours and Consultation Co. <?= date('Y') ?></p>
