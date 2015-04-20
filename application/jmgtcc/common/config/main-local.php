1b84bf51bc557f079f0781ebf0400f5c8906c62e
Erika Hidalgo <rekhidalgo23@gmail.com>
2015-04-20 14:13:23 +0800

    Updated db connection confid

--- application/jmgtcc/common/config/main-local.php
+++ application/jmgtcc/common/config/main-local.php
@@ -4,8 +4,8 @@ return [
         'db' => [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=jmgtcc_brs',
-            'username' => 'root',
-            'password' => '',
+            'username' => 'jmgtcc_brs',
+            'password' => 'jmgtcc_brs',
             'charset' => 'utf8',
         ],
     	'user' => [
